c5b17882a8bf4b6a98c28120aa2e04382d28c3b2
quaide <tranterq@gmail.com>
2019-10-19 21:53:25 -0400

    Added John's Registration fxml, Registration Controller, and updated the LogInController to start a new scene to the registration page

--- src/sample/LogInController.java
+++ src/sample/LogInController.java
@@ -1,15 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class Controller {
+public class LogInController {
     @FXML
     private TextField loginUsername;
 
@@ -18,11 +24,11 @@ public class Controller {
 
     DatabaseManager databaseManager;
 
-    public Controller() throws SQLException {
+    public LogInController() throws SQLException {
         databaseManager = new DatabaseManager();
     }
 
-    public void handleLoginButton(javafx.event.ActionEvent actionEvent) {
+    public void handleLoginButton(javafx.event.ActionEvent actionEvent) throws IOException {
         String userName = loginUsername.getText();
         String password = loginPassword.getText();
 
@@ -35,5 +41,15 @@ public class Controller {
 
         System.out.println(userName);
         System.out.println(password);
+    }
+
+    public void handleSignInButton(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Registration.fxml"));
+        Scene signInScene = new Scene(root);
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(signInScene);
+        window.show();
     }
 }
---
+++ src/sample/RegistrationController.java
@@ -0,0 +1,207 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.*;
+
+public class RegistrationController implements Initializable {
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private PasswordField passMain;
+
+    @FXML
+    private PasswordField passConfirm;
+
+    @FXML
+    private RadioButton btnDriver;
+
+    @FXML
+    private RadioButton btnRider;
+
+    @FXML
+    private Label txtPasswordNotMatch;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private AnchorPane areaDriver;
+
+    @FXML
+    private ComboBox<?> chcStateCar;
+
+    @FXML
+    private TextField txtDriverNum;
+
+    @FXML
+    private TextField txtLicensePlate;
+
+    @FXML
+    private TextField txtYear;
+
+    @FXML
+    private TextField txtModel;
+
+    @FXML
+    private TextField txtMake;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private TextField txtCreditCardNum;
+
+    @FXML
+    private ComboBox<?> chcMonth;
+
+    @FXML
+    private ComboBox<?> chcYear;
+
+    @FXML
+    private TextField txtCVV;
+
+    @FXML
+    private ComboBox<?> chcStateBilling;
+
+    @FXML
+    private TextField txtAddress1;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtAddress2;
+
+    @FXML
+    private TextField txtZip;
+
+    @FXML
+    private Label txtMissingItems;
+
+    @FXML
+    void checkPasswordMatch(KeyEvent event) {
+        if(passMain.getText().equals(passConfirm.getText()) ){
+            txtPasswordNotMatch.setVisible(false);
+        } else {
+            txtPasswordNotMatch.setVisible(true);
+        }
+    }
+
+    @FXML
+    void enableCarInfoSection(ActionEvent event) {
+        if(btnDriver.isSelected()){
+            areaDriver.setDisable(false);
+        } else {
+            areaDriver.setDisable(true);
+        }
+    }
+
+    @FXML
+    void submitForm(ActionEvent event) {
+        boolean nameEmpty= txtName.getText() == null || txtName.getText().trim().isEmpty();
+        boolean userNameEmpty = txtUserName.getText() == null || txtUserName.getText().trim().isEmpty();
+        boolean passwordEmpty = txtPasswordNotMatch.isVisible() || passMain.getText() == null ||
+                passMain.getText().trim().isEmpty() || passConfirm.getText() == null || passConfirm.getText().trim().isEmpty();
+        boolean userTypeNotSelected = (!btnDriver.isSelected() && !btnRider.isSelected());
+        boolean firstNameEmpty = txtFirstName.getText() == null || txtFirstName.getText().trim().isEmpty();
+        boolean lastNameEmpty = txtLastName.getText() == null || txtLastName.getText().trim().isEmpty();
+        boolean creditCardNumEmpty = txtCreditCardNum.getText() == null || txtCreditCardNum.getText().trim().isEmpty();
+        boolean monthEmpty = chcMonth.getSelectionModel().isEmpty();
+        System.out.println(monthEmpty);
+        System.out.println("name " + nameEmpty);
+        System.out.println("type " + userTypeNotSelected);
+        boolean yearCardEmpty = chcYear.getSelectionModel().isEmpty();
+        boolean cvvEmpty = txtCVV.getText() == null || txtCVV.getText().trim().isEmpty();
+        boolean address1Empty = txtAddress1.getText() == null || txtAddress1.getText().trim().isEmpty();
+        boolean cityEmpty = txtCity.getText() == null || txtCity.getText().trim().isEmpty();
+        boolean stateBillEmpty = chcStateBilling.getSelectionModel().isEmpty();
+        boolean zipEmpty = txtZip.getText() == null || txtZip.getText().trim().isEmpty();
+        if(!(nameEmpty||userNameEmpty||passwordEmpty||userTypeNotSelected||firstNameEmpty||lastNameEmpty||creditCardNumEmpty||monthEmpty||yearCardEmpty||cvvEmpty||address1Empty||cityEmpty||stateBillEmpty||zipEmpty)){
+            txtMissingItems.setVisible(false);
+            completeRegistration();
+            final Node source = (Node) event.getSource();
+            final Stage stage = (Stage) source.getScene().getWindow();
+            stage.close(); //closes the window
+        } else {
+            txtMissingItems.setVisible(true);
+        }
+
+    }
+
+    public void completeRegistration(){
+        String name = txtName.getText();
+        String userName = txtUserName.getText();
+        String password = passMain.getText();
+        String userType;
+        if(btnRider.isSelected()){
+            userType = "Rider";
+        } else if(btnDriver.isSelected()){
+            userType = "Driver";
+        } else if(btnDriver.isSelected()&&btnRider.isSelected()){
+            userType = "Driver and Rider";
+        } else {
+            userType = "N/A";
+        }
+        String firstName = txtFirstName.getText();
+        String lastName = txtLastName.getText();
+        String creditCardNumber = txtCreditCardNum.getText(); //String for now
+        int monthCard = (Integer) chcMonth.getValue();
+        int yearCard = (Integer) chcYear.getValue();
+        String cvv = txtCVV.getText(); //String for now
+        String address1 = txtAddress1.getText();
+        String address2 = txtAddress2.getText();
+        String city = txtCity.getText();
+        String state = (String) chcStateBilling.getValue();
+        String zip = txtZip.getText(); //String for now
+
+    }
+
+    public void initialize(URL url, ResourceBundle resource) {
+        List<Integer> monthList = new ArrayList<>();
+        for(int x=1;x<=12;x++){
+            monthList.add(x);
+        }
+        ObservableList month = FXCollections.observableArrayList(monthList);
+        chcMonth.getItems().addAll(month);
+        List<Integer> yearList = new ArrayList<>();
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        for (int y = year; y<(year +20); y++ ){
+            yearList.add(y);
+        }
+        ObservableList yearObsList = FXCollections.observableArrayList(yearList);
+        chcYear.getItems().addAll(yearObsList);
+        String states[] = {"AL","AK","AS","AZ","AR","CA","CO","CT","DE","DC","FM","FL","GA","GU","HI","ID","IL","IN",
+                "IA","KS","KY","LA","ME","MH","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC",
+                "ND","MP","OH","OK","OR","PW","PA","PR","RI","SC","SD","TN","TX","UT","VT","VI","VA","WA","WV","WI",
+                "WY"};
+        List<String> statesList = new ArrayList<>();
+        for(int x = 0; x<59;x++){
+            statesList.add(states[x]);
+        }
+        System.out.println(states.length);
+        ObservableList statesObsList = FXCollections.observableArrayList(statesList);
+        chcStateBilling.getItems().addAll(statesObsList);
+        chcStateCar.getItems().addAll(statesObsList);
+    }
+
+
+}
