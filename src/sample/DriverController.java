5cdb36a53fcaf3bcade820496dfae37d3370edef
Karan Patel <42840476+Kharnate@users.noreply.github.com>
2019-11-09 12:20:39 -0500

    Update DriverController.java

--- src/sample/DriverController.java
+++ src/sample/DriverController.java
@@ -7,109 +7,112 @@ import javafx.scene.control.Label;
 public class DriverController {
 
 
-  @FXML
-  private Label nameLabel;
+   @FXML
+    private Label nameLabel;
 
-  @FXML
-  private Label phoneLabel;
+    @FXML
+    private Label phoneLabel;
 
-  @FXML
-  private Label creditCardLabel;
+    @FXML
+    private Label creditCardLabel;
 
-  @FXML
-  private Label profileRatings;
+    @FXML
+    private Label profileRatings;
 
-  @FXML
-  private Label totalOAC;
+    @FXML
+    private Label totalOAC;
 
-  @FXML
-  private Button btnEditName;
+    @FXML
+    private Button btnEditName;
 
-  @FXML
-  private Button btnEditPhone;
+    @FXML
+    private Button btnEditPhone;
 
-  @FXML
-  private Button btnEditCreditCard;
+    @FXML
+    private Button btnEditCreditCard;
 
-  @FXML
-  private Label tripPaymentLabel;
+    @FXML
+    private Button btnLogOut;
 
-  @FXML
-  private Label totalTipLabel;
+    @FXML
+    private Label tripPaymentLabel;
 
-  @FXML
-  private Label totalEarnedLabel;
+    @FXML
+    private Label totalTipLabel;
 
-  @FXML
-  private Button btnSelectTripOne;
+    @FXML
+    private Label totalEarnedLabel;
 
-  @FXML
-  private Button btnSelectTripTwo;
+    @FXML
+    private Button btnSelectTripOne;
 
-  @FXML
-  private Button btnSelectTripThree;
+    @FXML
+    private Button btnSelectTripTwo;
 
-  @FXML
-  private Label selectTripNameOne;
+    @FXML
+    private Button btnSelectTripThree;
 
-  @FXML
-  private Label selectTripNameTwo;
+    @FXML
+    private Label selectTripNameOne;
 
-  @FXML
-  private Label selectTripThree;
+    @FXML
+    private Label selectTripNameTwo;
 
-  @FXML
-  private Label selectTripDestinationOne;
+    @FXML
+    private Label selectTripThree;
 
-  @FXML
-  private Label selectTripDestinationTwo;
+    @FXML
+    private Label selectTripDestinationOne;
 
-  @FXML
-  private Label selectTripDestinationThree;
+    @FXML
+    private Label selectTripDestinationTwo;
 
-  @FXML
-  private Label selectTripAddressOne;
+    @FXML
+    private Label selectTripDestinationThree;
 
-  @FXML
-  private Label selectTripAddressTwo;
+    @FXML
+    private Label selectTripAddressOne;
 
-  @FXML
-  private Label selectTripAddressThree;
+    @FXML
+    private Label selectTripAddressTwo;
 
-  @FXML
-  private Label historyNameOne;
+    @FXML
+    private Label selectTripAddressThree;
 
-  @FXML
-  private Label historyAddressOne;
+    @FXML
+    private Label historyNameOne;
 
-  @FXML
-  private Label historyPaymentOne;
+    @FXML
+    private Label historyAddressOne;
 
-  @FXML
-  private Label historyTipOne;
+    @FXML
+    private Label historyPaymentOne;
 
-  @FXML
-  private Label historyNameTwo;
+    @FXML
+    private Label historyTipOne;
 
-  @FXML
-  private Label historyAddressTwo;
+    @FXML
+    private Label historyNameTwo;
 
-  @FXML
-  private Label historyPaymentTwo;
+    @FXML
+    private Label historyAddressTwo;
 
-  @FXML
-  private Label historyTipTwo;
+    @FXML
+    private Label historyPaymentTwo;
 
-  @FXML
-  private Label historyNameThree;
+    @FXML
+    private Label historyTipTwo;
 
-  @FXML
-  private Label historyAddressThree;
+    @FXML
+    private Label historyNameThree;
 
-  @FXML
-  private Label historyPaymentThree;
+    @FXML
+    private Label historyAddressThree;
 
-  @FXML
-  private Label historyTipThree;
+    @FXML
+    private Label historyPaymentThree;
+
+    @FXML
+    private Label historyTipThree;
 
 }
