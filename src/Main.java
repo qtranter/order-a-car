f8eac4b6cfd1a34cef672492c62a0e2cc380334c
quaide <tranterq@gmail.com>
2019-12-04 16:58:47 -0500

    updatinng code with moving parts

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,3 +22,31 @@ public class Main extends Application {
         launch(args);
     }
 }
+=======
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("test.fxml"));
+        Parent root = loader.load();
+        loader.getController();
+        primaryStage.getIcons().add(new Image("https://i.imgur.com/HRn7QCk.png"));
+        primaryStage.setScene(new Scene(root, 450, 525));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+>>>>>>> Stashed changes
